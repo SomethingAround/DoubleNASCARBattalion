a8619af07c75f8c50d1e4152f049f3d55ede630d
Connor <connor.li.1597@outlook.com>
2020-05-04 15:40:13 +1000

    Created Turret and Bullet
    Added basic turret shooting stuff for turret and started the bullet script and also added a new folder in scripts for the turret scripts

---
+++ Double NASCAR Battalion/Assets/Scripts/Player/Turret/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	public Transform m_turret;
+	Rigidbody m_rb;
+    // Start is called before the first frame update
+    void Awake()
+    {
+		m_turret = gameObject.GetComponent<Transform>().parent;
+		m_rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
