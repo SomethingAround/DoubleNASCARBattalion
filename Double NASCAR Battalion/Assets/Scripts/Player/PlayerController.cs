97a41045d0b7415fea29049d5a8050ab4c6696c3
James <james.goutos16@gmail.com>
2020-05-05 10:22:31 +1000

    Added very basic player controller movement

---
+++ Double NASCAR Battalion/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class PlayerController : MonoBehaviour
+{
+    public int playerID = 4;
+    public float playerSpeed = 5;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (XCI.IsPluggedIn(playerID))
+        {
+            if (XCI.GetAxis(XboxAxis.LeftStickX, (XboxController)playerID) != 0 || XCI.GetAxis(XboxAxis.LeftStickX, (XboxController)playerID) != 0)
+            {
+                Vector3 inputDirection = new Vector3(XCI.GetAxis(XboxAxis.LeftStickX, (XboxController)playerID), 0, XCI.GetAxis(XboxAxis.LeftStickY, (XboxController)playerID));
+                inputDirection.Normalize();
+
+                rb.transform.localRotation = Quaternion.LookRotation(inputDirection, Vector3.up);
+                rb.velocity = transform.forward * playerSpeed;
+            }
+        }
+    }
+}
